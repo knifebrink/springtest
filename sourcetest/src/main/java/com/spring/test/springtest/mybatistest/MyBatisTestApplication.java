f29cff553985128d388cc36ae5c302253c275de5
brink <867547286@qq.com>
2021-11-15 20:08:44 +0800

    增加mybatis测试

--- sourcetest/src/main/java/com/spring/test/springtest/mybatistest/MyBatisTestApplication.java
+++ sourcetest/src/main/java/com/spring/test/springtest/mybatistest/MyBatisTestApplication.java
@@ -2,9 +2,16 @@ package com.spring.test.springtest.mybatistest;
 
 import org.apache.ibatis.binding.MapperProxy;
 import org.apache.ibatis.executor.SimpleExecutor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * @author brink
@@ -13,8 +20,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class MyBatisTestApplication {
 
+
     public static void main(String[] args){
         ConfigurableApplicationContext applicationContext = SpringApplication.run(MyBatisTestApplication.class,args);
+        MyBatisTestApplication myBatisTestApplication = applicationContext.getBean(MyBatisTestApplication.class);
         TalkUserMapper talkUserMapper = applicationContext.getBean(TalkUserMapper.class);
         System.out.println(talkUserMapper);
         talkUserMapper.selectAll();
@@ -29,11 +38,35 @@ public class MyBatisTestApplication {
 
         TalkUserBean talkUserBean = applicationContext.getBean(TalkUserBean.class);
         talkUserBean.toString();
+        myBatisTestApplication.test();
         applicationContext.stop();
+
+    }
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+//    @PostConstruct
+    public void test(){
+        // sss
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        TalkUserMapper talkUserMapper = sqlSession.getMapper(TalkUserMapper.class);
+        talkUserMapper.selectAll();
+        TalkUserBean talkUserBean = talkUserMapper.selectByName("brink");
+        System.out.println(sqlSession);
+        System.out.println(talkUserBean);
     }
 
     private static void someClass(){
         MapperProxy mapperProxy = null;
         SimpleExecutor simpleExecutor;
+        BoundSql boundSql;
+        SqlSession sqlSession;
+    }
+
+    /**
+     *
+     */
+    private static void buzhou(){
+
     }
 }
